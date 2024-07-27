4d57329b3878ec230b51522ed0c29f67b0d2e458
Paulo César Conduru <133069658+pcconduru@users.noreply.github.com>
2024-07-27 20:07:20 -0300

    Atualização de Código

--- app/src/main/java/com/example/composepage/MainActivity.kt
+++ app/src/main/java/com/example/composepage/MainActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composepage.ui.theme.ComposePageTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,26 +31,59 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposePageTheme {
-
+                Surface (
+                    modifier = Modifier.fillMaxSize()
+                ){
+                    PageImage()
+                }
             }
         }
     }
 }
 
 @Composable
-fun PageText(title: String, t1: String, t2: String) {
+fun PageText(title: String, t1: String, t2: String, modifier: Modifier = Modifier) {
     Column(modifier = Modifier){
         Text(
-            text = title
+            text = title,
+            fontSize = 24.sp,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp)
         )
-        Text(text = t1)
-        Text(text = t2)
+        Text(
+            text = t1,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = t2,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp)
+        )
     }
 }
 @Preview(showBackground = true)
 @Composable
 fun PagePreview() {
     ComposePageTheme {
-        PageText(title = "TEST1", t1 = "TEST2" , t2 = "TEST3")
+        PageImage()
+    }
+}
+@Composable
+fun PageImage(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        PageText(
+            title = stringResource(R.string.title_compose_page),
+            t1 = stringResource(R.string.t1_compose_page),
+            t2 = stringResource(R.string.t2_compose_page)
+        )
     }
 }
